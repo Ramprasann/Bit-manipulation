b1141d7839769a0907fc665ae667fa33fbcf878c
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-09-03 23:13:09 +0530

    set ith bit

---
+++ setithbit.java
@@ -0,0 +1,10 @@
+public class setithbit {
+    public static int setinbitt(int n,int i){
+        int bitmask=1<<i;
+        return n| bitmask;
+    }
+    public static void main(String[] args) {
+        System.out.println(setinbitt(10, 2));
+    }
+    
+}
